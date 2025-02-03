d3417e3e998a80cd7e3dd40cd4f6bfe2802d4cfd
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-03 23:22:21 +0330

    feat(router): add route

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
     <>
-
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<>f</>} />
+            </Routes>
+        </BrowserRouter>
     </>
 )
